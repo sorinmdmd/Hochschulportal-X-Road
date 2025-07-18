aa7f7adb8b470b5c78d9d3f8c242725ea323ad15
unknown <otelsorin@gmail.com>
2025-07-18 18:04:10 +0200

    MongoDB init

--- hsbobackend/src/main/kotlin/edu/hsbo/hsbobackend/controller/StudentController.kt
+++ hsbobackend/src/main/kotlin/edu/hsbo/hsbobackend/controller/StudentController.kt
@@ -1,15 +1,25 @@
 package edu.hsbo.hsbobackend.controller
 
+import edu.hsbo.hsbobackend.entities.Student
+import edu.hsbo.hsbobackend.service.StudentService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class StudentController {
+class StudentController(val studentService: StudentService) {
 
     @GetMapping("/students")
     fun getStudents(): String {
-        return "Hello World!"
+        return studentService.getAllStudents().toString()
+    }
+
+    @PostMapping("/students")
+    fun addStudent(@RequestBody student: Student): Student {
+       return studentService.addStudent(student)
     }
 }
--- hsbobackend/src/main/kotlin/edu/hsbo/hsbobackend/service/StudentService.kt
+++ hsbobackend/src/main/kotlin/edu/hsbo/hsbobackend/service/StudentService.kt
@@ -1,6 +1,17 @@
 package edu.hsbo.hsbobackend.service
 
-class StudentService {
+import edu.hsbo.hsbobackend.entities.Student
+import edu.hsbo.hsbobackend.repository.StudentRepository
+import org.springframework.stereotype.Service
 
+@Service
+class StudentService(val studentRepository: StudentRepository) {
 
+    fun getAllStudents(): List<Student> {
+        return studentRepository.findAll()
+    }
+
+    fun addStudent(student: Student): Student {
+       return studentRepository.save(student)
+    }
 }
