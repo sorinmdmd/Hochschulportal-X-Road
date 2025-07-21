66373910ca4504a159522d7a5c4cd38671b9d646
unknown <otelsorin@gmail.com>
2025-07-21 23:14:14 +0200

    Added

--- akafoewg/src/main/kotlin/edu/hsbo/akafoewg/repository/HousingUnitOccupancyRepository.kt
+++
@@ -1,9 +0,0 @@
-package edu.hsbo.akafoewg.repository
-
-import edu.hsbo.akafoewg.entities.HousingUnitOccupancy
-import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface HousingUnitOccupancyRepository: MongoRepository<HousingUnitOccupancy, String> {
-}
--- akafoewg/src/main/kotlin/edu/hsbo/akafoewg/service/WaitlistService.kt
+++
@@ -1,8 +0,0 @@
-package edu.hsbo.akafoewg.service
-
-import edu.hsbo.akafoewg.repository.WaitlistRepository
-import org.springframework.stereotype.Service
-
-@Service
-class WaitlistService (val waitlistRepository: WaitlistRepository) {
-}
